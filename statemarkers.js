a54f6ad1cb4659d049adf144752aa210b47fc77e
GPNikol <gregorypnikol@gmail.com>
2018-05-06 21:05:09 -0400

    Manages large JS arrays referenced in the directory

---
+++ statemarkers.js
@@ -0,0 +1,414 @@
+
+var stateMarkerContent = [
+{
+	coords:{lat:32.806671,lng:-86.791130},
+	title:'Alabama',
+	markType:'state',
+	state_id:'AL'
+	},
+	{
+	coords:{lat:61.370716,lng:-152.404419},
+	title:'Alaska',
+	markType:'state',
+	state_id:'AK'
+	},
+	{
+	coords:{lat:33.729759,lng:-111.431221},
+	title:'Arizona',
+	markType:'state',
+	state_id:'AZ'
+	},
+	{
+	coords:{lat:34.969704,lng:-92.373123},
+	title:'Arkansas',
+	markType:'state',
+	state_id:'AR'
+	},
+	{
+	coords:{lat:36.116203,lng:-119.681564},
+	title:'California',
+	markType:'state',
+	state_id:'CA'
+	},
+	{
+	coords:{lat:39.059811,lng:-105.311104},
+	title:'Colorado',
+	markType:'state',
+	state_id:'CO'
+	},
+	{
+	coords:{lat:41.597782,lng:-72.755371},
+	title:'Connecticut',
+	markType:'state',
+	state_id:'CT'
+	},
+	{
+	coords:{lat:39.318523,lng:-75.507141},
+	title:'Delaware',
+	markType:'state',
+	state_id:'DE'
+	},
+	{
+	coords:{lat:38.897438,lng:-77.026817},
+	title:'Washington D.C.',
+	markType:'state',
+	state_id:'DC'
+	},
+	{
+	coords:{lat:27.766279,lng:-81.686783},
+	title:'Florida',
+	markType:'state',
+	state_id:'FL'
+	},
+	{
+	coords:{lat:33.040619,lng:-83.643074},
+	title:'Georgia',
+	markType:'state',
+	state_id:'GA'
+	},
+	{
+	coords:{lat:21.094318,lng:-157.498337},
+	title:'Hawaii',
+	markType:'state',
+	state_id:'HI'
+	},
+	{
+	coords:{lat:44.240459,lng:-114.478828},
+	title:'Idaho',
+	markType:'state',
+	state_id:'ID'
+	},
+	{
+	coords:{lat:40.349457,lng:-88.986137},
+	title:'Illinois',
+	markType:'state',
+	state_id:'IL'
+	},
+	{
+	coords:{lat:39.849426,lng:-86.258278},
+	title:'Indiana',
+	markType:'state',
+	state_id:'IN'
+	},
+	{
+	coords:{lat:42.011539,lng:-93.210526},
+	title:'Iowa',
+	markType:'state',
+	state_id:'IA'
+	},
+	{
+	coords:{lat:38.526600,lng:-96.726486},
+	title:'Kansas',
+	markType:'state',
+	state_id:'KS'
+	},
+	{
+	coords:{lat:37.668140,lng:-84.670067},
+	title:'Kentucky',
+	markType:'state',
+	state_id:'KY'
+	},
+	{
+	coords:{lat:31.169546,lng:-91.867805},
+	title:'Louisiana',
+	markType:'state',
+	state_id:'LA'
+	},
+	{
+	coords:{lat:44.693947,lng:-69.381927},
+	title:'Maine',
+	markType:'state',
+	state_id:'ME'
+	},
+	{
+	coords:{lat:39.063946,lng:-76.802101},
+	title:'Maryland',
+	markType:'state',
+	state_id:'MD'
+	},
+	{
+	coords:{lat:42.230171,lng:-71.530106},
+	title:'Massachusetts',
+	markType:'state',
+	state_id:'MA'
+	},
+	{
+	coords:{lat:43.326618,lng:-84.536095},
+	title:'Michigan',
+	markType:'state',
+	state_id:'MI'
+	},
+	{
+	coords:{lat:45.694454,lng:-93.900192},
+	title:'Minnesota',
+	markType:'state',
+	state_id:'MN'
+	},
+	{
+	coords:{lat:32.741646,lng:-89.678696},
+	title:'Mississippi',
+	markType:'state',
+	state_id:'MS'
+	},
+	{
+	coords:{lat:38.456085,lng:-92.288368},
+	title:'Missouri',
+	markType:'state',
+	state_id:'MO'
+	},
+	{
+	coords:{lat:46.921925,lng:-110.454353},
+	title:'Montana',
+	markType:'state',
+	state_id:'MT'
+	},
+	{
+	coords:{lat:41.125370,lng:-98.268082},
+	title:'Nebraska',
+	markType:'state',
+	state_id:'NE'
+	},
+	{
+	coords:{lat:38.313515,lng:-117.055374},
+	title:'Nevada',
+	markType:'state',
+	state_id:'NV'
+	},
+	{
+	coords:{lat:43.452492,lng:-71.563896},
+	title:'New Hampshire',
+	markType:'state',
+	state_id:'NH'
+	},
+	{
+	coords:{lat:40.298904,lng:-74.521011},
+	title:'New Jersey',
+	markType:'state',
+	state_id:'NJ'
+	},
+	{
+	coords:{lat:34.840515,lng:-106.248482},
+	title:'New Mexico',
+	markType:'state',
+	state_id:'NM'
+	},
+	{
+	coords:{lat:42.165726,lng:-74.948051},
+	title:'New York',
+	markType:'state',
+	state_id:'NY'
+	},
+	{
+	coords:{lat:35.630066,lng:-79.806419},
+	title:'North Carolina',
+	markType:'state',
+	state_id:'NC'
+	},
+	{
+	coords:{lat:47.528912,lng:-99.784012},
+	title:'North Dakota',
+	markType:'state',
+	state_id:'ND'
+	},
+	{
+	coords:{lat:40.388783,lng:-82.764915},
+	title:'Ohio',
+	markType:'state',
+	state_id:'OH'
+	},
+	{
+	coords:{lat:35.565342,lng:-96.928917},
+	title:'Oklahoma',
+	markType:'state',
+	state_id:'OK'
+	},
+	{
+	coords:{lat:44.572021,lng:-122.070938},
+	title:'Oregon',
+	markType:'state',
+	state_id:'OR'
+	},
+	{
+	coords:{lat:40.590752,lng:-77.209755},
+	title:'Pennsylvania',
+	markType:'state',
+	state_id:'PA'
+	},
+	{
+	coords:{lat:41.680893,lng:-71.511780},
+	title:'Rhode Island',
+	markType:'state',
+	state_id:'RI'
+	},
+	{
+	coords:{lat:33.856892,lng:-80.945007},
+	title:'South Carolina',
+	markType:'state',
+	state_id:'SC'
+	},
+	{
+	coords:{lat:44.299782,lng:-99.438828},
+	title:'South Dakota',
+	markType:'state',
+	state_id:'SD'
+	},
+	{
+	coords:{lat:35.747845,lng:-86.692345},
+	title:'Tennessee',
+	markType:'state',
+	state_id:'TN'
+	},
+	{
+	coords:{lat:31.054487,lng:-97.563461},
+	title:'Texas',
+	markType:'state',
+	state_id:'TX'
+	},
+	{
+	coords:{lat:40.150032,lng:-111.862434},
+	title:'Utah',
+	markType:'state',
+	state_id:'UT'
+	},
+	{
+	coords:{lat:44.045876,lng:-72.710686},
+	title:'Vermont',
+	markType:'state',
+	state_id:'VT'
+	},
+	{
+	coords:{lat:37.769337,lng:-78.169968},
+	title:'Virginia',
+	markType:'state',
+	state_id:'VA'
+	},
+	{
+	coords:{lat:47.400902,lng:-121.490494},
+	title:'Washington',
+	markType:'state',
+	state_id:'WA'
+	},
+	{
+	coords:{lat:38.491226,lng:-80.954453},
+	title:'West Virginia',
+	markType:'state',
+	state_id:'WV'
+	},
+	{
+	coords:{lat:44.268543,lng:-89.616508},
+	title:'Wisconsin',
+	markType:'state',
+	state_id:'WI'
+	},
+	{
+	coords:{lat:42.755966,lng:-107.302490},
+	title:'Wyoming',
+	markType:'state',
+	state_id:'WY'
+	}
+];
+
+
+var parkMarkerContent = [
+	{
+	coords:{lat:37.230873,lng:-108.461838},
+	content:'<div id="content">'+
+		'<div id="content-center">'+
+		'<h2>Mesa Verde, CO</h2>'+
+		'<img id="iw-image" src="images/mesa-verde.jpg">'+
+		'</div>'+
+		'</div>',
+	state_id:'CO'
+	},
+	{
+	coords:{lat:29.610300,lng:-94.449081},
+	content:'<div id="content">'+
+		'<div id="content-center">'+
+		'<h2>Anahuac Wildlife Refuge, Anahuac, TX</h2>'+
+		'<img id="iw-image" src="images/anahuac-refuge.jpg">'+
+		'</div>'+
+		'</div>',
+	state_id:'TX'
+	},
+	{
+	coords:{lat:44.427895,lng:-110.588379},
+	content:'<div id="content">'+
+		'<div id="content-center">'+
+		'<h2>Yellow Stone, WY</h2>'+
+		'<img id="iw-image" src="images/yellow-stone.jpg">'+
+		//'<button id="button-size" type="submit" value="Submit">Press</button>'+
+		'</div>'+
+		'</div>',
+	state_id:'WY'
+	},
+	{
+	coords:{lat:40.720585,lng:-77.826965},
+	content:'<div id="content">'+
+		'<div id="content-center">'+
+		'<h2>Rothrock State Forest, State College, PA</h2>'+
+		'<img id="iw-image" src="images/rothrock-state.jpg">'+
+		'</div>'+
+		'</div>',
+	state_id:'PA'
+	},
+	{
+	coords:{lat:37.317207,lng:-113.022537},
+	content:'<div id="content">'+
+		'<div id="content-center">'+
+		'<h2>Zion, UT</h2>'+
+		'<img id="iw-image" src="images/zion.jpg">'+
+		'</div>'+
+		'</div>',
+	state_id:'UT'
+	},
+	{
+	coords:{lat:37.865101,lng:-119.538330},
+	content:'<div id="content">'+
+		'<div id="content-center">'+
+		'<h2>Yosemite, CA</h2>'+
+		'<img id="iw-image" src="images/yosemite-mt.jpg">'+
+		'</div>'+
+		'</div>',
+	state_id:'CA'
+	},
+	{
+	coords:{lat:38.235195,lng:-120.066483},
+	content:'<div id="content">'+
+		'<div id="content-center">'+
+		'<h2>Stanislaus, Camp Connell, CA</h2>'+
+		'<img id="iw-image" src="images/camp-connell.jpg">'+
+		'</div>'+
+		'</div>',
+	state_id:'CA'
+	},
+	{
+	coords:{lat:39.0145,lng:-76.3999},
+	content:'<div id="content">'+
+		'<div id="content-center">'+
+		'<h2>Sandy Point State Park, MD</h2>'+
+		'<img id="iw-image" src="images/sandy-point.jpg">'+
+		'</div>'+
+		'</div>',
+	state_id:'MD'
+	},
+	{
+	coords:{lat:36.1070,lng:-112.1130},
+	content:'<div id="content">'+
+		'<div id="content-center">'+
+		'<h2>Grand Canyon National Park, AZ</h2>'+
+		'<img id="iw-image" src="images/grandcanyon.jpg">'+
+		'</div>'+
+		'</div>',
+	state_id:'AZ'
+	},
+	{
+	coords:{lat:35.6532,lng:-83.5070},
+	content:'<div id="content">'+
+		'<div id="content-center">'+
+		'<h2>Great Smokey Mountains, TN</h2>'+
+		'<img id="iw-image" src="images/greatsmokeymountains.jpg">'+
+		'</div>'+
+		'</div>',
+	state_id:'TN'
+	}
+];
