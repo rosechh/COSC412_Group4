d7aa9e4a14f1759fc57aff57ff5f81acb5b541eb
Andrew Knickman <andrewsknick@gmail.com>
2018-05-08 19:41:16 -0400

    National Parks Game v0.3
    National parks game with base game assets, asset templates, test assets,
    scripts (includes script outline), etc.
    Currently cutting sprite sheets, animating sprites, and assigning sprite
    customization ID's for save of player game state in database.

---
+++ National Parks Game/COSC 412 National Parks Game/Assets/National Park Assets/Scripts/CycleSprites.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CycleSprites : MonoBehaviour
+{
+    public Sprite sp1;
+    public Sprite sp2;
+    public Sprite sp3;
+    Sprite locSp;
+ 
+	void Start ()
+    {
+        Button btn = gameObject.GetComponent<Button>();
+        locSp = gameObject.GetComponent<Sprite>();
+        btn.onClick.AddListener(Cycle);
+	}
+	void Cycle ()
+    {
+        if (locSp == sp1)
+            locSp = sp2;
+        else if (locSp == sp2)
+            locSp = sp3;
+        else
+            locSp = sp1;
+	}
+}
---
+++ National Parks Game/COSC 412 National Parks Game/Assets/National Park Assets/Scripts/SoundManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource fxSource;
+    public AudioSource musSource;
+    public static SoundManager instance = null;
+
+    //+- 5% of original pitch for sound variation
+    public float lowPitchRange = .95f;
+    public float highPitchRange = 1.05f;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        //keeps music between scenes
+        DontDestroyOnLoad(gameObject);
+	}
+
+    //public to call from other scripts that execute game logic
+    public void PlaySingle(AudioClip clip)
+    {
+        fxSource.clip = clip;
+        fxSource.Play();
+
+    }
+
+    //Input comma list of arguments of type clip i.e. clip array
+    public void RandomFX(params AudioClip[] clips)
+    {
+        int randomIndex = Random.Range(0, clips.Length);
+        float randomPitch = Random.Range(lowPitchRange, highPitchRange);
+
+        fxSource.pitch = randomPitch;
+        fxSource.clip = clips[randomIndex];
+        fxSource.Play();
+
+    }
+}
