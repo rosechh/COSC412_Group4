b97251d2c35c91ee4ba4b0ba3020f79d7c746579
rosechh <36388340+rosechh@users.noreply.github.com>
2018-05-22 16:59:37 -0400

    Update getStates.php

--- getStates.php
+++ getStates.php
@@ -1,26 +1,20 @@
 <?php
 
-	$username = "ssingh19";
-	$password = "Group4123!";
-	$hostname = "localhost";
-	$database = "NatParks";
-	
-	$conn = mysqli_connect($hostname, $username, $password) or die("Unable to connect to MySQL");
-	mysql_select_db( $conn, $database) or die("Could not select examples");
-
-    $query = "SELECT name FROM `States`";
+    require "opendb.php";
+    
+    $query = "SELECT name FROM States";
     $data = mysqli_query($conn, $query);
 
-    $makes = array();
+    $States = array();
 
     while($row = mysqli_fetch_array($data))
     {
         array_push($States, $row["name"]);
-        
+       
+        echo "state: " . $row["name"] . "<br>";
+        echo json_encode($States);
     }
-    echo json_encode($States);
+    
+    require "closedb.php";
 
-    
-    
-	mysqli_close($conn);
 ?>
