9c2acc82dfea5feaba07aa289d7898fd770b84ae
rosechh <36388340+rosechh@users.noreply.github.com>
2018-05-22 17:04:33 -0400

    Update dynamicDD.js

--- dynamicDD.js
+++ dynamicDD.js
@@ -1,30 +1,24 @@
-$(document).ready(function() {
- $.getJSON("getStates.php", success = function(data)
-{
-    var options = " ";
-    for(var i = 0; i < data.length; i++)
-    {
-        options += "<options value =' " +  data[i].toLowerCase() + "'>" + data[i] + "</option>";
-    }
+newFunction();
 
-    $("slctStates").append(options);
-    $("slctStates").change();
-});
-$("slctStates").change(function()
-{
-    $.getJSON("getParks.php?States =" + $(this).val(), success = function(data)
-    {
-        var options = " ";
-        for(var i = 0; i < data.length; i++)
-        {
-            options += "<options value =' " +  data[i].toLowerCase() + "'>" + data[i] + "</option>";
-        }
-        
-        $("slctPark").html("");
-        $("slctPark").append(options);
-    });  
-
-});
-
-
-});
+function newFunction() {
+    $(document).ready(function () {
+        $.getJSON("getStates.php", success = function (data) {
+            var options = " ";
+            for (var i = 0; i < data.length; i++) {
+                options += "<options value =' " + data[i].toLowerCase() + "'>" + data[i] + "</option>";
+            }
+            $("slctStates").append(options);
+            $("slctStates").change();
+        });
+        $("slctStates").change(function () {
+            $.getJSON("getParks.php?States =" + $(this).val(), success = function (data) {
+                var options = " ";
+                for (var i = 0; i < data.length; i++) {
+                    options += "<options value =' " + data[i].toLowerCase() + "'>" + data[i] + "</option>";
+                }
+                $("slctPark").html("");
+                $("slctPark").append(options);
+            });
+        });
+    });
+}
