1c35363cfeddcec1303e2ab7cf31e38fead21d26
GPNikol <gregorypnikol@gmail.com>
2018-05-22 11:59:06 -0400

    Added more park markers in Colorado for presentation

--- statemarkers.js
+++ statemarkers.js
@@ -315,6 +315,8 @@ var parkMarkerContent = [
 	content:'<div id="content">'+
 		'<div id="content-center">'+
 		'<h2>Mesa Verde, CO</h2>'+
+		'<div>Mesa Verde, Spanish for green table, offers a spectacular look into the lives of the Ancestral Pueblo people who made it their home for over 700 years, from AD 600 to 1300. Today the park protects nearly 5,000 known archeological sites, including 600 cliff dwellings. These sites are some of the most notable and best preserved in the United States. </div>'+
+		'<br>'+
 		'<img id="iw-image" src="images/mesa-verde.jpg">'+
 		'</div>'+
 		'</div>',
@@ -410,5 +412,149 @@ var parkMarkerContent = [
 		'</div>'+
 		'</div>',
 	state_id:'TN'
+	},//==============================================PRESENTATION MARKERS====================================================================\\
+	{
+	coords:{lat:38.5754,lng:-107.7416},
+	content:'<div id="content">'+
+		'<div id="content-center">'+
+		'<h2>Black Canyon of The Gunnison, CO</h2>'+
+		'<div>Big enough to be overwhelming, still intimate enough to feel the pulse of time, Black Canyon of the Gunnison exposes you to some of the steepest cliffs, oldest rock, and craggiest spires in North America. With two million years to work, the Gunnison River, along with the forces of weathering, has sculpted this vertical wilderness of rock, water, and sky. </div>'+
+		'<br>'+
+		'<img id="iw-image" src="images/Black_Canyon.jpg">'+
+		'</div>'+
+		'</div>',
+	state_id:'CO'
+	},
+	{
+	coords:{lat:40.3428,lng:-105.6836},
+	content:'<div id="content">'+
+		'<div id="content-center">'+
+		'<h2>Rocky Mountain, CO</h2>'+
+		'<div>Rocky Mountain National Park’s 415 square miles encompass and protect spectacular mountain environments. Enjoy Trail Ridge Road – which crests at over 12,000 feet including many overlooks to experience the subalpine and alpine worlds – along with over 300 miles of hiking trails, wildflowers, wildlife, starry nights, and fun times. In a world of superlatives, Rocky is on top! </div>'+
+		'<br>'+
+		'<img id="iw-image" src="images/rocky_mountain.jpg">'+
+		'</div>'+
+		'</div>',
+	state_id:'CO'
+	},
+	{
+	coords:{lat:39.0575,lng:-108.6938},
+	content:'<div id="content">'+
+		'<div id="content-center">'+
+		'<h2>Colorado National Monument, CO</h2>'+
+		'<div>Colorado National Monument preserves one of the grand landscapes of the American West. But this treasure is much more than a monument. Towering monoliths exist within a vast plateau and canyon panorama. You can experience sheer-walled, red rock canyons along the twists and turns of Rim Rock Drive, where you may spy bighorn sheep and soaring eagles. </div>'+
+		'<br>'+
+		'<img id="iw-image" src="images/co_nat_mon.jpg">'+
+		'</div>'+
+		'</div>',
+	state_id:'CO'
+	},
+	{
+	coords:{lat:37.7916,lng:-105.5943},
+	content:'<div id="content">'+
+		'<div id="content-center">'+
+		'<h2>Great Sand Dunes, CO</h2>'+
+		'<div>The tallest dunes in North America are the centerpiece in a diverse landscape of grasslands, wetlands, conifer and aspen forests, alpine lakes, and tundra. Experience this diversity through hiking, sand sledding, splashing in Medano Creek, wildlife watching, and more! The park and preserve are always open, so plan to also experience night skies and nocturnal wildlife during your visit.</div>'+
+		'<br>'+
+		'<img id="iw-image" src="images/great_sand_dunes.jpg">'+
+		'</div>'+
+		'</div>',
+	state_id:'CO'
+	},
+	{
+	coords:{lat:37.1944,lng:-107.3012},
+	content:'<div id="content">'+
+		'<div id="content-center">'+
+		'<h2>Chimney Rock, CO</h2>'+
+		'<div>This undiscovered gem is an intimate, off-the-beaten-path archaeological site located at the southern edge of the San Juan Mountains  in Southwestern Colorado. You\'ll walk in the footsteps of the fascinating and enigmatic Ancestral Puebloans of the Chaco clan, following primitive pathways that haven\'t changed for 1,000 years. Archaeological ruins and artifacts, abundant wildlife, and its setting in the breathtaking San Juan National Forest make Chimney Rock a must-see.</div>'+
+		'<br>'+
+		'<img id="iw-image" src="images/chimney_rock.jpg">'+
+		'</div>'+
+		'</div>',
+	state_id:'CO'
+	},
+	{
+	coords:{lat:40.5464379,lng:-106.7219809},
+	content:'<div id="content">'+
+		'<div id="content-center">'+
+		'<h2>Medicine Bow-Routt & Thunder Basin, CO</h2>'+
+		'<div>The Routt National Forest, initially created by President Theodore Roosevelt as the Park Range Forest Reserve in 1905, was established to manage range lands and for watershed protection for stockmen and farmers using this area. In 1908 the name Routt replaced Park Range, in honor of the first elected governor of the State. Headquarters for the Reserve was in Kremmling until 1907 when it was moved to Steamboat Springs, the same year the Reserve was substantially enlarged. Over 100,000 acres of agricultural lands were eliminated from the Reserve in 1910 by proclamation of President Taft. By 1948 the Forest totaled over a million acres.</div>'+
+		'<br>'+
+		'<img id="iw-image" src="images/medicine_bow_routt.jpg">'+
+		'</div>'+
+		'</div>',
+	state_id:'CO'
+	},
+	{
+	coords:{lat:37.5439,lng:-107.764},
+	content:'<div id="content">'+
+		'<div id="content-center">'+
+		'<h2>San Juan, CO</h2>'+
+		'<div>Located approximately 30 miles southeast of San Juan, El Yunque National Forest is the sole tropical rain forest in the U.S. National Forest System. The forest’s relatively small 28,000 acre size belies its importance. It differs significantly from all the other U.S. National Forests because of its year-round tropical climate and immense biodiversity.</div>'+
+		'<br>'+
+		'<img id="iw-image" src="images/san_juan.jpg">'+
+		'</div>'+
+		'</div>',
+	state_id:'CO'
+	},
+	{
+	coords:{lat:37.3380205,lng:-108.6018524},
+	content:'<div id="content">'+
+		'<div id="content-center">'+
+		'<h2>Hovenweep Monument, CO</h2>'+
+		'<div>Once home to over 2,500 people, Hovenweep includes six prehistoric villages built between A.D. 1200 and 1300. Explore a variety of structures, including multistory towers perched on canyon rims and balanced on boulders. The construction and attention to detail will leave you marveling at the skill and motivation of the builders.</div>'+
+		'<br>'+
+		'<img id="iw-image" src="images/hovenweep.jpg">'+
+		'</div>'+
+		'</div>',
+	state_id:'CO'
+	},
+	{
+	coords:{lat:39.4374848,lng:-106.8130944},
+	content:'<div id="content">'+
+		'<div id="content-center">'+
+		'<h2>White River Forest, CO</h2>'+
+		'<div>The White River Plateau Timber Land Reserve, approximately 1.2 million acres, was established by the proclamation of President Benjamin Harrison in 1891, but it wasn\'t until 1898 that funding for the administration of the Reserve was appropriated by Congress. The area, being highly valued by stockmen, was initially administered from Meeker. But by 1917 the headquarters were moved to Glenwood Springs and housed in the same building that the offices for the Holy Cross Forest were in. For much of the next couple of decades grazing and timber management occupied Forest administration.</div>'+
+		'<br>'+
+		'<img id="iw-image" src="images/white_river.jpg">'+
+		'</div>'+
+		'</div>',
+	state_id:'CO'
+	},
+	{
+	coords:{lat:37.2493,lng:-108.6865},
+	content:'<div id="content">'+
+		'<div id="content-center">'+
+		'<h2>Yucca House Monument, CO</h2>'+
+		'<div>Through a continuing tradition of public and private cooperation, Yucca House National Monument preserves one of the largest archeological sites in SW Colorado. The unexcavated nature of the site preserves its integrity and beauty for future generations of scientists and visitors. Experience a sense of discovery by visiting a site that has remained largely untouched for the past 800 years! </div>'+
+		'<br>'+
+		'<img id="iw-image" src="images/yucca_house.JPG">'+
+		'</div>'+
+		'</div>',
+	state_id:'CO'
+	},
+	{
+	coords:{lat:38.9136173,lng:-105.2854076},
+	content:'<div id="content">'+
+		'<div id="content-center">'+
+		'<h2>Florissant Fossil Beds, CO</h2>'+
+		'<div>Beneath a grassy mountain valley in central Colorado lies one of the richest and most diverse fossil deposits in the world. Petrified redwood stumps up to 14 feet wide and thousands of detailed fossils of insects and plants reveal the story of a very different, prehistoric Colorado. </div>'+
+		'<br>'+
+		'<img id="iw-image" src="images/florissant_fossils.jpg">'+
+		'</div>'+
+		'</div>',
+	state_id:'CO'
+	},
+	{
+	coords:{lat:37.3657,lng:-108.858},
+	content:'<div id="content">'+
+		'<div id="content-center">'+
+		'<h2>Canyon of The Ancients, CO</h2>'+
+		'<div>The Monument contains the highest known archaeological site density in the United States, with rich, well-preserved evidence of native cultures. The archeological record etched into this landscape is much more than isolated islands of architecture. This cultural landscape contains more than 6,355 recorded sites reflect all the physical components of past human life: villages, field houses, check dams, reservoirs, great kivas, cliff dwellings, shrines, sacred springs, agricultural fields, petroglyphs and sweat lodges. Some areas have more than 100 sites per square mile. The number of sites is estimated to be up to 30,000.</div>'+
+		'<br>'+
+		'<img id="iw-image" src="images/canyon_of_ancients.JPG">'+
+		'</div>'+
+		'</div>',
+	state_id:'CO'
 	}
 ];
