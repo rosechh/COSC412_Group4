6585d7443988bac60c019f46790fefe0afa901cc
rosechh <36388340+rosechh@users.noreply.github.com>
2018-05-22 17:08:35 -0400

    Update opendb.php

--- opendb.php
+++ opendb.php
@@ -1,4 +1,5 @@
 <?php
+require "configure.php";
 $conn = mysqli_connect($hostname, $username, $password) or die("Unable to connect to MySQL");
 	mysqli_select_db( $conn, $database) or die("Could not select examples");
 ?>
